85b6eba087718d26f7dea71a446c15dbf0a98e78
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-10 11:21:56 +0500

    Add files via upload

---
+++ string assignment.py
@@ -0,0 +1,32 @@
+#Task no.1 characters
+char1='A'
+char2='7'
+char3= '#'
+print( char1, char2, char3)
+#Task no.2  Strings
+#create two string variables:
+animal= "Eagle"
+quality= " The Eagles are strong and powerful."
+print(animal)
+print(quality)
+#Task_no.3 Concatenation
+fname= "Amna"
+lname= " Rehman"
+print( fname + lname)
+#Task_on.4 Repetition
+string2="Hi"
+i='Hi\n'
+print(5*i)
+#Task_no.5 Indexing
+name="PythonRocks"
+print(name)
+print(name[0])
+print(name[3])
+print(name[10])
+#Task_no.6 Slicing
+string="l love coding in python."
+substr1=string[2:6]
+substr2= string[17:23]
+print(substr1)
+print(substr2)
+
