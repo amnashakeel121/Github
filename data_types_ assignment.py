8405a01774241159708009be317f5361caac71a9
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-10 09:20:55 +0500

    Add files via upload

---
+++ data_types_ assignment.py
@@ -0,0 +1,39 @@
+#Integer
+# what will be your age in 5 years.
+age= 30
+print("In 5 year, you will be", age+5)
+#String
+#create variable name with real name
+#create greeting using "Hello,"with your name
+name= "Amna"
+greeting= " Hello," + name +"!"
+print(greeting)
+#Float
+#create variable height and store your height in meters.
+height=1.52 #height in meters.
+print("Your height is 1.52 in meters.")
+#Combined use of string, integer, and float.
+Item="Book"
+Quantity=4 #(Integer)
+price_per_item= 12.99 #(Float)
+#Calculate the total cost(quantity *price).
+print("You bought a 4 Books for a total of 51.96 dollars.")
+#Let's building a shopping program.
+#Ask the user to enter
+name= " Shakil"
+item= "pencil"
+price_per_item= 5 # price in dollers
+quantity= 20
+print("Hello Shakil! You bought 20 pencils.Total 100 dollars.")
+
+
+
+
+
+
+
+
+
+
+
+
