87e1a69c733f222f5cfd13daa1ea71be38122429
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-12 22:07:19 +0500

    Add files via upload

---
+++ arithmetic_assignment.py
@@ -0,0 +1,56 @@
+#Additiom
+x=7
+y=5
+result =x +y
+print(result)
+print(f"{x}+{y}={result}")
+# Substraction
+x=7
+y=5
+result =x -y
+print(result)
+print(f"{x}-{y}={result}")
+# Mutiplication
+x=7
+y=5
+result =x *y
+print(result)
+print(f"{x}*{y}={result}")
+#Division
+x=7
+y=5
+result =x /y
+print(result)
+print(f"{x}/{y}={result}")
+#Floor Division
+x=7
+y=5
+result =x// y
+print(result)
+print(f"{x}//{y}={result}")
+#Modulus
+x=7
+y=5
+result = x%y
+print(result)
+print(f"{x}%{y}={result}")
+# Exponentiation
+x=7
+y=5
+result =x **y
+print(result)
+print(f"{x}**{y}={result}")
+
+
+
+ 
+    
+=[['Operator', 'Name', 'Example', 'Result']
+            ['+','Addition','7+5','12']
+            ['-', 'Substraction', '7-5','2']
+            ['*', 'Multiplication', '7*5', '35']
+            ['/', 'Division', 7/5, '1.4']
+            ['//', 'Floor Division', '7//5','1']
+            ['%', 'Modulus', '7%5', '2']
+            ['**', 'Exponentiation','7**5','16807']]
+print(table_data)
