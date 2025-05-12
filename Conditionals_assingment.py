de394d2b99e9dd0e0aef42bec1f713a0562ce46f
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-12 19:22:26 +0500

    Add files via upload

---
+++ Conditionals_assingment.py
@@ -0,0 +1,55 @@
+#Task no.1
+age=18
+if age>=18:
+         print("You are and adult")
+# Task no.2
+age=15
+if age>=18:
+        print("You are an adult")
+else:
+    print("You are nor and adult")
+#Task no.3
+marks=75
+if marks>=90:
+         print(" Grade A")
+elif marks>=70:
+         print("Grade B")
+elif marks>=50:
+      print("Grade C")
+else:
+      print("Fail")  
+#Task no.4
+#weather = "rainy","sunny" ,or "cloudy".
+if "rainy" :
+    print("Take an Umberalla.")
+if "sunny":
+     print("Wear sunglasses")
+if "cloudy":
+    print("It might rain later")
+#Task no.5    
+#"hungry"= True, or False.
+if "hungry" :
+     print("Go eat something! ")
+else:
+   print("Maybe have a snake later.")
+#Task no. 5 
+#mood = input("How are you feeling today?")
+if "happy":
+      print("That's great to hear!")
+if "sad":
+    print(" I hope your day gets better.")
+if "bored":
+   print("Try reading a book or going for walk.")  
+
+
+
+   
+    
+    
+     
+ 
+           
+     
+      
+         
+
