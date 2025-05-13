41112f824359b0c8fd971f27fe4c098fbf8f9c59
Amna shakil <hafizaaminarehman@gmail.com>
2025-05-13 10:55:45 +0500

    Add files via upload

--- string assignment.py
+++ string assignment.py
@@ -11,7 +11,7 @@ print(animal)
 print(quality)
 #Task_no.3 Concatenation
 fname= "Amna"
-lname= " Rehman"
+lname= "Rehman"
 full_name= fname+" "+ lname
 print(full_name)
 #Task_on.4 Repetition
@@ -19,21 +19,26 @@ string2="Hi"
 i='Hi\n'
 print(5*i)
 #Task_no.5 Indexing
-text="PythonRocks"
-print(text)
-print(text[0])
-print(text[3])
-print(text[10])
+word="PythonRocks"
+print(word)
+print(word[0])
+print(word[3])
+print(word[10])
 #Task_no.6 Slicing
-string="l love coding in python."
-substr1=string[2:6]
-substr2= string[17:23]
+text="l love coding in python."
+substr1=text[2:6]
+substr2= text[17:23]
 print(substr1)
 print(substr2)
 #Bonus task
 name= " zara"
 greeting= "Welcome"+ name+"!."
 print(greeting*3)
+
+
+
+
+3)
 
 
 
